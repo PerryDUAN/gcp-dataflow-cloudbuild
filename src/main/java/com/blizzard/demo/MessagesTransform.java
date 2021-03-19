b00db21545755c761ebe21cfbaa02add4d5ad365
pduan <pduan@blizzard.com>
2021-03-19 12:41:50 -0700

    dataflowpipeline: reset to original one

--- src/main/java/com/blizzard/demo/MessagesTransform.java
+++ src/main/java/com/blizzard/demo/MessagesTransform.java
@@ -18,12 +18,12 @@ public class MessagesTransform extends DoFn<PubsubMessage, PubsubMessage>{
     public void process(ProcessContext context) {
         LOG.info(String.format("Received message %s", new String(context.element().getPayload())));
         PubsubMessage msg = context.element();
-        Map<String, String> attributes = new HashMap<>();
-        attributes.put("routeID", "dummy");
-        attributes.put("traceRoute", "fake");
-        attributes.put("messageName", "test");
-        PubsubMessage newMsg = new PubsubMessage(msg.getPayload(), attributes);
+//        Map<String, String> attributes = new HashMap<>();
+//        attributes.put("routeID", "dummy");
+//        attributes.put("traceRoute", "fake");
+//        attributes.put("messageName", "test");
+//        PubsubMessage newMsg = new PubsubMessage(msg.getPayload(), attributes);
 
-        context.output(newMsg);
+        context.output(msg);
     }
 }
