06cbb09fea0accf34e8ee822edc03e7200eb632d
pduan <pduan@blizzard.com>
2021-03-19 09:17:49 -0700

    DataflowPipelineOption changed

--- src/main/java/com/blizzard/demo/DataflowPipelineOption.java
+++ src/main/java/com/blizzard/demo/DataflowPipelineOption.java
@@ -9,20 +9,17 @@ import org.apache.beam.sdk.options.Default;
 public interface DataflowPipelineOption extends PipelineOptions, StreamingOptions {
 	@Description("Pub/Sub topic to read from. " + "Name format should look like: "
 			+ "projects/<project-id>/topics/<input-topic-name>. (project-id is default now)")
-//	@Default.String("${pubsub.default.input.topic}")
 	@Required
 	String getInputTopic();
 	void setInputTopic(String inputTopic);
 
 	@Description("Pub/Sub topic to publish to. " + "Name format should look like"
 			+ "projects/<project-id>/topics/<output-topic-name>. (project-id is default now)")
-//	@Default.String("${pubsub.default.output.topic}")
 	@Required
 	String getOutputTopic();
 	void setOutputTopic(String outputTopic);
 
 	@Description("GCP project name")
-//	@Default.String("${pubsub.default.output.topic}")
 	@Required
 	String getProject();
 	void setProject(String outputTopic);
