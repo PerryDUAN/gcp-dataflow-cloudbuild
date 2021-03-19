698445a0bab90ab4fc8ea8ed2b48641ee4049f62
pduan <pduan@blizzard.com>
2021-03-19 15:24:53 -0700

    updated datapipeline

---
+++ src/main/java/com/blizzard/demo/MessagesTransform.java
@@ -0,0 +1,30 @@
+package com.blizzard.demo;
+
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MessagesTransform extends DoFn<PubsubMessage, PubsubMessage> {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOG = LoggerFactory.getLogger(MessagesTransform.class);
+
+    @ProcessElement
+    public void process(ProcessContext context) {
+        LOG.info(String.format("Received message %s", new String(context.element().getPayload())));
+        PubsubMessage msg = context.element();
+        Map<String, String> attributes = new HashMap<>();
+        attributes.put("routeID", "dummy");
+        attributes.put("traceRoute", "fake");
+        attributes.put("messageName", "test1");
+        PubsubMessage newMsg = new PubsubMessage(msg.getPayload(), attributes);
+        LOG.info(String.format("New message attribute routeID %s", newMsg.getAttribute("routeID")));
+        context.output(newMsg);
+    }
+}
